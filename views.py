d9573c30ccf646bded419d24b959fea3a87fa3a0
HAFSAnCR7 <52420901+HAFSAnCR7@users.noreply.github.com>
2019-07-25 00:18:53 -0400

    Update views.py

--- views.py
+++ views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 def index(request):
     """The home page for Pizzeria."""
-    return render(request, 'pizzas/index.html')
+    return render(request, 'pizzas/index.html') # The render() function here uses two arguments—the original request object and a template it can use to build the page
