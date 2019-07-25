e62d554dc6d07c931c32d5405497f90527344251
HAFSAnCR7 <52420901+HAFSAnCR7@users.noreply.github.com>
2019-07-25 00:22:23 -0400

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from pizzas.models import Pizza, Topping
+from pizzas.models import Pizza, Topping # This code imports the models we want to register, Pizza and Topping
 
-admin.site.register(Pizza)
-admin.site.register(Topping)
+admin.site.register(Pizza) # to tell Django to manage our model through the admin site
+admin.site.register(Topping) # to tell Django to manage our model through the admin site 
 
 
