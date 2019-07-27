2ae3f6b21f919f8a51383958af09b80e2dee3dcf
HAFSAnCR7 <52420901+HAFSAnCR7@users.noreply.github.com>
2019-07-27 11:36:02 -0400

    Update secondurls.py

--- secondurls.py
+++ secondurls.py
@@ -1,12 +1,12 @@
 """Defines URL patterns for pizzas.""" #  we add a docstring to make it clear, which urls.py we’re working in at the beginning of the file
 
-from django.urls import path # this imports the url function, which is needed when mapping URLs to views
+from django.urls import path # have to use path instead of url or will not work in new versions of Django # the same concept, this imports the url function, which is needed when mapping URLs to views
 
 from . import views # the dot tells Python to import views from the same directory as the current urls.py module
 
 app_name = 'pizzas' # need this or will not work in Django 2.2
 urlpatterns = [ # The variable urlpatterns in this module is a list of individual pages that can be requested from the pizzas app
     # Home page
-    path('', views.index, name='index'), # In its entirety, this expression tells Python to look for a URL with nothing between the beginning and end of the URL
+    path('', views.index, name='index'),  # need to use path bc of new version of Django but same concept if it was url # In its entirety, this expression tells Python to look for a URL with nothing between the beginning and end of the URL
 #  the name index provides a link to the home page so we’ll use this name instead of writing out a URL
  ]
